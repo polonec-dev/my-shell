a2c541e0ddb8f27f17f593897d6667b44de54374
Polonec <polonec.devel@pm.me>
2026-07-14 16:08:31 +0200

    shell: restructure and add cd command

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
 
 #include "shell.hpp"
 
-CShell::CShell(std::string pr)
-: prompt_(pr) {}
+CShell::CShell(std::string user)
+: user_(user)
+{
+	cd("~");
+}
+CShell::CShell(std::string user, std::string dir)
+: user_(user)
+{
+	cd(dir);
+}
 void CShell::prompt()
 {
-	std::cout << prompt_ << std::flush;
+	std::cout << user_ + " " + dir_ + "> " << std::flush;
+}
+
+void CShell::cd(std::string path)
+{
+	if (path == ("/home/" + user_ )) {
+		path = "~";
+	}
+	dir_ = path;
+}
+
+int CShell::run(CCommand & command)
+{
+	return 0;
 }
--- src/shell.hpp
+++ src/shell.hpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
 
+#include "command.hpp"
+
 class CShell
 {
 private:
-	std::string prompt_;
+	std::string dir_;
+	std::string user_;
 public:
-	CShell(std::string pr);
+	CShell(std::string user);
+	CShell(std::string user, std::string dir);
 	void prompt();
+	void cd(std::string path);
+	int run(CCommand & command);
 };
